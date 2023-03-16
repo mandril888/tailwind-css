7fb093a591c110713fef565b89a75d68a7b04855
Arturo Rubio <arubio@seocom.agency>
2023-03-16 13:59:04 +0100

    style text

--- pages/typo.vue
+++ pages/typo.vue
@@ -1,7 +1,7 @@
 <template>
-    <h1>Title 1</h1>
+    <h1 class="itail underline decoration-dotted underline-offset-4">Title 1</h1>
     <h2>Title 2</h2>
-    <h3>Title 3</h3>
+    <h3 class="overline decoration-red-500 decoration-4">Title 3</h3>
     <p class="text-base">Regular paragraph</p>
     <p>Description paragraph</p>
     <p class="note text-xs">Little note <span>paragraph</span></p>
