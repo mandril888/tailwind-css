78b8bf74840b0857435da65b4f1b6ebd9730470d
Arturo Rubio <arubio@seocom.agency>
2023-03-28 16:35:18 +0200

    padding and margins

--- pages/sizes.vue
+++ pages/sizes.vue
@@ -1,4 +1,10 @@
 <template>
     <h1>Sizes</h1>
-    
+    <div class="p-3 bg-green-500">
+      <div class="px-3 py-6 bg-red-500">aaa</div>
+      <div class="mx-3 my-6 bg-yellow-500">aaa</div>
+      <div class="pt-2 pr-4 pb-6 pl-8 bg-blue-500">aaa</div>
+      <div class="mt-2 mr-4 mb-6 ml-8 bg-gray-500">aaa</div>
+      <div class="-ml-8 bg-purple-500">aaa</div>
+    </div>
 </template>
