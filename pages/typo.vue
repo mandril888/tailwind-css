0a6a5a537265eb054f6d1b87b83d8442d7ceb5bd
Arturo Rubio <arubio@seocom.agency>
2023-03-28 16:13:05 +0200

    text styles

--- pages/typo.vue
+++ pages/typo.vue
@@ -3,6 +3,7 @@
     <h2>Title 2</h2>
     <h3 class="overline decoration-red-500 decoration-4">Title 3</h3>
     <p class="text-base">Regular paragraph</p>
-    <p>Description paragraph</p>
+    <p class="uppercase">Description paragraph</p>
     <p class="note text-xs">Little note <span>paragraph</span></p>
+    <p class="truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec convallis nisl, ut vulputate lacus. Nam ex turpis, dignissim congue volutpat eget, semper sit amet est. Donec ut felis nec est vestibulum molestie. Nulla porta imperdiet massa ac auctor. Aenean dignissim nunc id lacinia volutpat. Vestibulum eu rutrum diam. Duis eu mattis neque. Vestibulum quis placerat lacus, ac dapibus leo. Etiam nec est at nunc laoreet ultrices. In est sem, efficitur id massa congue, dignissim euismod libero. Aliquam accumsan nec nulla ac tempus. Cras vitae metus ac ante vestibulum dictum vitae eget tortor. Nunc sit amet neque purus. Suspendisse tincidunt dapibus mauris sed aliquam. Aliquam malesuada, lectus sit amet posuere fringilla, tellus orci tempus magna, vel condimentum mi sapien vel ante. Donec volutpat commodo gravida.</p>
 </template>
